711886b03059e4152acd7624a2f44ce5a6a85437
Mahnoor Saeed <115603624+mahnoorxsaeed@users.noreply.github.com>
2023-09-12 17:47:31 -0500

    Add files via upload

---
+++ Openable.java
@@ -0,0 +1,8 @@
+package edu.ou.cs2334.project4.interfaces;
+
+import java.io.File;
+import java.io.IOException;
+public interface Openable {
+
+	void open(File file) throws IOException;
+}
---
+++ Saveable.java
@@ -0,0 +1,7 @@
+package edu.ou.cs2334.project4.interfaces;
+
+import java.io.IOException;
+
+public interface Saveable {
+	void save(String filename) throws IOException;
+}
